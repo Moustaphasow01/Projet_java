61ea2363383485b403bdc59e721c6b49c2696835
Zazou0 <101133587+Zazou0@users.noreply.github.com>
2023-04-30 11:35:58 +0200

    Update Rouleau.java

--- src/Rouleau.java
+++ src/Rouleau.java
@@ -1,17 +1,27 @@
 package src;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 
 public class Rouleau {
     private ArrayList<Symbole> symboles;
+    private int taille;
 
-    public Rouleau(ArrayList<Symbole> symboles) {
-        this.symboles = symboles;
-        Collections.shuffle(this.symboles);
+    public Rouleau(int taille) {
+        this.taille = taille;
+        symboles = new ArrayList<>(taille);
     }
 
-    public Symbole getSymbole(int index) {
-        return symboles.get(index);
+    public void genererSymboles() {
+        String[] symbolesPossibles = {"A", "B", "C", "D", "E", "F", "G", "H", "Super", "Free", "Bonus"};
+        Random random = new Random();
+
+        for (int i = 0; i < taille; i++) {
+            symboles.add(new Symbole(symbolesPossibles[random.nextInt(symbolesPossibles.length)]));
+        }
+    }
+
+    public ArrayList<Symbole> getSymboles() {
+        return symboles;
     }
 }
