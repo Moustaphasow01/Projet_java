f3fc71d83f29e99d2084bb0a2fd5afa80f9f8879
Zazou0 <101133587+Zazou0@users.noreply.github.com>
2023-05-05 08:25:16 +0200

    Update Rouleau.java
    clean code

--- test/Rouleau.java
+++ test/Rouleau.java
@@ -4,32 +4,42 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Rouleau {
-    private ArrayList<Symbole> symboles;
-    private int taille;
+  private ArrayList < Symbole > symboles;
+  private int taille;
 
-    public Rouleau(int taille) {
-        this.taille = taille;
-        symboles = new ArrayList<>(taille);
+  public Rouleau(int taille) {
+    this.taille = taille;
+    symboles = new ArrayList < > (taille);
+  }
+
+  public void genererSymboles() {
+    Symbole[] symbolesPossibles = {
+      Symbole.COEUR,
+      Symbole.PIQUE,
+      Symbole.CARRE,
+      Symbole.TREFLE,
+      Symbole.CERISE,
+      Symbole.CITRON,
+      Symbole.ORANGE,
+      Symbole.CLOCHE,
+      Symbole.SUPER,
+      Symbole.FREE,
+      Symbole.BONUS
+    };
+
+    Random random = new Random();
+
+    for (int i = 0; i < taille; i++) {
+      symboles.add(symbolesPossibles[random.nextInt(symbolesPossibles.length)]);
     }
+  }
 
-    public void genererSymboles() {
-        Symbole[] symbolesPossibles = {Symbole.COEUR, Symbole.PIQUE, Symbole.CARRE, Symbole.TREFLE,
-                                       Symbole.CERISE, Symbole.CITRON, Symbole.ORANGE, Symbole.CLOCHE,
-                                       Symbole.SUPER, Symbole.FREE, Symbole.BONUS};
+  public ArrayList < Symbole > getSymboles() {
+    return symboles;
+  }
 
-        Random random = new Random();
-
-        for (int i = 0; i < taille; i++) {
-            symboles.add(symbolesPossibles[random.nextInt(symbolesPossibles.length)]);
-        }
-    }
-
-    public ArrayList<Symbole> getSymboles() {
-        return symboles;
-    }
-
-    public String getSymbole(int i) {
-        return symboles.get(i).name();
-    }
+  public String getSymbole(int i) {
+    return symboles.get(i).name();
+  }
 
 }
