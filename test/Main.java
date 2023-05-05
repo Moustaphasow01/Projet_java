84e30c64095444943bde4c484da2203e543f93bd
Zazou0 <101133587+Zazou0@users.noreply.github.com>
2023-05-05 08:24:32 +0200

    Update Main.java
    clean code

--- test/Main.java
+++ test/Main.java
@@ -3,60 +3,60 @@ package test;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        MachineASous machineASous = new MachineASous();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    MachineASous machineASous = new MachineASous();
 
-        System.out.println("Bienvenue dans la machine a sous !");
-        boolean continuerJeu = true;
+    System.out.println("Bienvenue dans la machine a sous !");
+    boolean continuerJeu = true;
 
-        while (continuerJeu) {
-            System.out.println("Votre solde est de : " + machineASous.getJoueur().getSolde());
-            System.out.println("Choisissez votre mise (2000, 4000, 6000, 10000) ou tapez 'q' pour quitter : ");
-            String choixMise = scanner.next();
-            int mise;
-            
-            switch (choixMise) {
-                case "2000":
-                    mise = 2000;
-                    break;
-                case "4000":
-                    mise = 4000;
-                    break;
-                case "6000":
-                    mise = 6000;
-                    break;
-                case "10000":
-                    mise = 10000;
-                    break;
-                case "q":
-                    continuerJeu = false;
-                    System.out.println("Merci d'avoir joue !");
-                    continue;
-                default:
-                    System.out.println("Choix invalide. Veuillez choisir une mise valide.");
-                    continue;
-            }
+    while (continuerJeu) {
+      System.out.println("Votre solde est de : " + machineASous.getJoueur().getSolde());
+      System.out.println("Choisissez votre mise (2000, 4000, 6000, 10000) ou tapez 'q' pour quitter : ");
+      String choixMise = scanner.next();
+      int mise;
 
-            int gains = machineASous.jouer(mise, machineASous.getJoueur());
-          
-            machineASous.getJoueur().ajouterGains(gains);
+      switch (choixMise) {
+      case "2000":
+        mise = 2000;
+        break;
+      case "4000":
+        mise = 4000;
+        break;
+      case "6000":
+        mise = 6000;
+        break;
+      case "10000":
+        mise = 10000;
+        break;
+      case "q":
+        continuerJeu = false;
+        System.out.println("Merci d'avoir joue !");
+        continue;
+      default:
+        System.out.println("Choix invalide. Veuillez choisir une mise valide.");
+        continue;
+      }
 
-            if (machineASous.isPartieBonus()) {
-                System.out.println("Vous avez declenche la partie bonus !");
+      int gains = machineASous.jouer(mise, machineASous.getJoueur());
 
-                // Demander au joueur de choisir une case
-                System.out.println("Choisissez une case (1-20) :");
-                int choix = scanner.nextInt();
-            }
+      machineASous.getJoueur().ajouterGains(gains);
 
-            // Si les tours gratuits sont déclenchés
-            if (machineASous.isToursGratuits()) {
-                // Ajoutez ici la logique pour gérer les tours gratuits, demander au joueur de choisir un nombre de tours, calculer les gains, etc.
-                // ...
-            }
-        }
+      if (machineASous.isPartieBonus()) {
+        System.out.println("Vous avez declenche la partie bonus !");
 
-        scanner.close();
+        // Demander au joueur de choisir une case
+        System.out.println("Choisissez une case (1-20) :");
+        int choix = scanner.nextInt();
+      }
+
+      // Si les tours gratuits sont déclenchés
+      if (machineASous.isToursGratuits()) {
+        //todo Ajoutez ici la logique pour gérer les tours gratuits, demander au joueur de choisir un nombre de tours, calculer les gains, etc.
+        // ...
+      }
     }
+
+    scanner.close();
+  }
 }
