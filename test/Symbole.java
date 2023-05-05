9b9b566d49a88702c8c018d46d404bebc204b00c
Zazou0 <101133587+Zazou0@users.noreply.github.com>
2023-05-05 08:26:09 +0200

    Update Symbole.java
    clean code

--- test/Symbole.java
+++ test/Symbole.java
@@ -1,18 +1,17 @@
 package test;
 
-
 public enum Symbole {
-	  COEUR,
-	  PIQUE,
-	  CARRE,
-	  TREFLE,
-	  CERISE,
-	  CITRON,
-	  ORANGE,
-	  CLOCHE,
-	  BAR,
-	  SEPT,
-	  SUPER,
-	  FREE,
-	  BONUS,
-	}
+  COEUR,
+  PIQUE,
+  CARRE,
+  TREFLE,
+  CERISE,
+  CITRON,
+  ORANGE,
+  CLOCHE,
+  BAR,
+  SEPT,
+  SUPER,
+  FREE,
+  BONUS,
+}
